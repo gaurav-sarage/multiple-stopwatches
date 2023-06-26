6fa8e21f4107e67ab3645951b40e2d81742d16e2
Gaurav <gauravsarage.1902@gmail.com>
2023-06-27 00:27:41 +0530

    added right component and added styles to the file

--- src/App.js
+++ src/App.js
@@ -72,12 +72,115 @@ pause: [] },
               <h1 style={{ fontWeight: "bold", fontSize: 30 }}>
                 StopWatch
               </h1>
+              <p style={{ fontWeight: "normal", fontSize: 20 }}>
+                Built by Gaurav
+              </p>
             </div>
+            <div style={{ cursor: "pointer", alignItems: "center", display: "flex", flexDirection: "row", gap: 10 }} onClick={() => setStopwatchData([...stopwatchData, { id: stopwatchData.length, name: `Timer ${stopwatchData.length + 1}`, time: 0, isRunning: false, lap: [], pause: [] }])}>
+              <BsStopwatch size={20} color={"white"} />
+              <p>Add StopWatch</p>
+            </div>
+          </div>
+
+          <p className="main-time-label">
+            {stopwatchData[selectedRow]?.time > 0 ? new Date(stopwatchData[selectedRow]?.time).toISOString().slice(11, -1) : "00:00:00.000"}
+          </p>
+
+          <div className="main-button-controls">
+            <button className={"control-buttons"} 
+              onClick={() => handleStart(stopwatchData[selectedRow].id)}
+            >
+              Start
+            </button>
+
+            <button className={"control-buttons"}
+              onClick={() => handleLap(stopwatchData[selectedRow].id)}
+            >
+              Lap
+            </button>
+
+            <button className={"control-buttons"}
+              onClick={() => handleStop(stopwatchData[selectedRow].id)}
+            >
+              Stop
+            </button>
+
+            <button className={"control-buttons"}
+              onClick={() => handleReset(stopwatchData[selectedRow].id)}
+            >
+              Reset
+            </button>
+          </div>
+          <p className="lap-label">
+            Laps
+          </p>
+          <div className="lap-container">
+            {stopwatchData[selectedRow]?.lap.length > 0 && 
+            stopwatchData[selectedRow]?.lap.map((lap, index) => (
+              <div style={{ display: "flex", justifyContent: "space-between", "width": "500px" }}>
+                <p> Lap {index + 1}</p>
+                <p className="lap-time-label" key={index}>
+                  {new Date(lap).toISOString().slice(11, -1)}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
 
         <div className="right">
+          {stopwatchData.map((stopwatch) => (
+            <div className="stopwatchContainer" key={stopwatch.id} style={{ cursor: "pointer" }} onClick={() => {
+              setSelectedRow(stopwatch.id)
+            }}>
+              <div className="stopwatchName">
+                <input className="name-label" value={stopwatch.name} onChange={(e) => {
+                  setStopwatchData(stopwatchData.map((stopwatchItem) => {
+                    if (stopwatchItem.id === stopwatch.id) {
+                      return { ...stopwatchItem, name: e.target.value}
+                    }
+                    return stopwatchItem
+                  }))
+                }} />
+              </div>
+              <div className="stopwatchTime">
+                <p className="time-label">
+                  {new Date(stopwatch.time).toISOString().slice(11, -1)}
+                </p>
+                <p>
+                  {stopwatch.lap.length} Laps
+                </p>
+              </div>
+              <div className="stopWatchButtons">
+                <button 
+                  className={"control-buttons"} 
+                  onClick={() => handleStart(stopwatch.id)}
+                >
+                  Start
+                </button>
 
+                <button 
+                  className={"control-buttons"} 
+                  onClick={() => handleLap(stopwatch.id)}
+                >
+                  Lap
+                </button>
+
+                <button 
+                  className={"control-buttons"} 
+                  onClick={() => handleStop(stopwatch.id)}
+                >
+                  Stop
+                </button>
+
+                <button 
+                  className={"control-buttons"} 
+                  onClick={() => handleReset(stopwatch.id)}
+                >
+                  Reset
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
